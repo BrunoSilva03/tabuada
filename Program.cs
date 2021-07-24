c41047b14259182db26926eb847fa80fc24e4955
BrunoSilva03 <78625466+BrunoSilva03@users.noreply.github.com>
2021-07-24 10:27:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Algoritmo que faz a tabuada básica(até 10) de qualquer número
+            //autor: Bruno Silva Rodrigues
+
+
+            int contador = 0, numero, resultado;
+
+            Console.WriteLine("Digite um número");
+            numero = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            do
+            {
+                resultado = numero * contador;
+                Console.WriteLine("{0} X {1} = {2}", numero, contador, resultado);
+                Console.WriteLine("\t");
+                contador++;
+            } while (contador <= 10);
+
+
+            Console.ReadKey();
+        }
+    }
+}
